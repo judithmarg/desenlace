ab5d8cc216b9f9528fb31b1dd36ced7389d92cd6
judithMarg <judithmargaritapacollalli@gmail.com>
2024-06-08 02:26:03 -0400

    config project

---
+++ firebaseConfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+
+// Optionally import the services that you want to use
+// import {...} from "firebase/auth";
+// import {...} from "firebase/database";
+// import {...} from "firebase/firestore";
+// import {...} from "firebase/functions";
+// import {...} from "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  appId: process.env.APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
